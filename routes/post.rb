9c890229dd11f197bf858f9112e97288ac9d1ff6
Lasse Brandt <lasse@bitmand.com>
2015-11-23 17:44:40 +0100

    A little bit of code cleanup
    Absolutely not difference in functionality.

--- routes/post.rb
+++ routes/post.rb
@@ -1,8 +1,8 @@
 class MisterHyde < Sinatra::Application
 
     get '/post/list' do
-        @posts = Post.all( session[:site_id] )
-        erb :'post/list'
+        posts = Post.all( session[:site_id] )
+        erb :'post/list', :locals => { :posts => posts }
     end
 
     get '/post/create' do
@@ -27,22 +27,23 @@ class MisterHyde < Sinatra::Application
     end
 
     get '/post/edit/:id' do
-        @site = Site.get( session[:site_id] )
-        @post = Post.get( session[:site_id], params['id'] )
-        erb :'post/edit', :locals => { :errors => [] }
+        site = Site.get( session[:site_id] )
+        post = Post.get( session[:site_id], params['id'] )
+        erb :'post/edit', :locals => { :site => site, :post => post }
     end
 
     post '/post/edit/:id' do
-        @site = Site.get( session[:site_id] )
-        @post = Post.get( session[:site_id], params['id'] )
-        @post.settings['title'] = params['title'].strip
-        @post.settings['published'] = params['published'].strip
-        @post.settings['layout'] = params['layout'].strip
-        @post.settings['date'] = params['date'].strip
-        @post.content = params['content']
-        @post.save
-        Commandline.git_add( @site.id, @post.git_filename , 'Updating post: ' + @post.id.shellescape )
-        erb :'post/edit', :locals => { :errors => @post.error_messages }
+        site = Site.get( session[:site_id] )
+        post = Post.get( session[:site_id], params['id'] )
+        post.settings['title'] = params['title'].strip
+        post.settings['published'] = params['published'].strip
+        post.settings['layout'] = params['layout'].strip
+        post.settings['date'] = params['date'].strip
+        post.content = params['content']
+        if post.save
+            Commandline.git_add( site.id, post.git_filename , 'Updating post: ' + post.id.shellescape )
+        end
+        erb :'post/edit', :locals => { :site => site, :post => post }
     end
 
     get '/post/delete/:id' do
